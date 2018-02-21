f1d26fafff8ffeb9977e33c27d05ade7a0cff767
zhangsheng377 <435878393@qq.com>
2018-02-21 23:01:11 +0800

    完成DBN预测空气

--- deep-belief-network-master/deep-belief-network-master/examples/regression_demo.py
+++ deep-belief-network-master/deep-belief-network-master/examples/regression_demo.py
@@ -2,7 +2,7 @@ import numpy as np
 
 np.random.seed(1337)  # for reproducibility
 from sklearn.datasets import load_boston
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.metrics.regression import r2_score, mean_squared_error
 from sklearn.preprocessing import MinMaxScaler
 import os
@@ -12,37 +12,68 @@ import xlrd
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from dbn.tensorflow import SupervisedDBNRegression
 
-
-#from dbn. import SupervisedDBNClassification
+# from dbn. import SupervisedDBNClassification
 
 data = xlrd.open_workbook('data.xls')
 table = data.sheet_by_index(0)
-pm25=table.col_values(2)[1:]
-pm25=np.array(pm25)
-temperature=table.col_values(8)[1:]
-temperature=np.array(temperature)
-wind=table.col_values(9)[1:]
-wind=np.array(wind)
-moisture=table.col_values(11)[1:]
-moisture=np.array(moisture)
-weather=table.col_values(10)[1:]
-weather=np.array(weather)
+data_num = table.nrows - 1
+pm25 = table.col_values(2)[1:]
+pm25 = np.array(pm25)
+temperature = table.col_values(8)[1:]
+temperature = np.array(temperature)
+wind = table.col_values(9)[1:]
+wind = np.array(wind)
+moisture = table.col_values(11)[1:]
+moisture = np.array(moisture)
+weather = table.col_values(10)[1:]
+weather = np.array(weather)
 
-Data=np.concatenate((pm25,temperature,wind,weather,moisture),axis=0)
-Data=Data.reshape((5,738))
+use_min_max_scaler = True  # must use True
+use_all_data = True
+step = 1
 
-print (Data)
+if use_all_data:
+    Data = np.concatenate((pm25[0:step], temperature[0:step], wind[0:step], weather[0:step], moisture[0:step]), axis=0)
+else:
+    Data = pm25[0:step]
+Target = pm25[step]
+print(Data.shape)
+print(Target.shape)
+for i in range(step + 1, data_num):
+    if use_all_data:
+        Data = np.row_stack((Data, np.concatenate(
+            (pm25[i - step:i], temperature[i - step:i], wind[i - step:i], weather[i - step:i], moisture[i - step:i]),
+            axis=0)))
+    else:
+        Data = np.row_stack((Data, pm25[i - step:i]))
+    Target = np.row_stack((Target, pm25[i]))
+X = Data
+Y = Target[:, 0]
+print(X.shape)
+print(Y.shape)
+
 '''
-# Loading dataset
-boston = load_boston()
-X, Y = boston.data, boston.target
+Data = np.concatenate((pm25, temperature, wind, weather, moisture), axis=0)
+Data = Data.reshape((738, 5))
+print(Data.shape)
+print(pm25[0].shape)
+X = Data
+Y = pm25
+'''
 
+'''
+    # Loading dataset
+    boston = load_boston()
+    X, Y = boston.data, boston.target
+ '''
 # Splitting data
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=1337)
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=1337)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=1337)
 
 # Data scaling
 min_max_scaler = MinMaxScaler()
-X_train = min_max_scaler.fit_transform(X_train)
+if use_min_max_scaler:
+    X_train = min_max_scaler.fit_transform(X_train)
 
 # Training
 regressor = SupervisedDBNRegression(hidden_layers_structure=[100],
@@ -55,7 +86,7 @@ regressor = SupervisedDBNRegression(hidden_layers_structure=[100],
 regressor.fit(X_train, Y_train)
 
 # Test
-X_test = min_max_scaler.transform(X_test)
+if use_min_max_scaler:
+    X_test = min_max_scaler.transform(X_test)
 Y_pred = regressor.predict(X_test)
 print('Done.\nR-squared: %f\nMSE: %f' % (r2_score(Y_test, Y_pred), mean_squared_error(Y_test, Y_pred)))
-'''

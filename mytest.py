df4594dd6039dc6bc8dd10e8e1deafe57bfee6ed
zhangsheng377 <435878393@qq.com>
2018-03-09 00:10:13 +0800

    做了分段看正确率来调整学习速率的算法，发现不收敛

--- mytest.py
+++ mytest.py
@@ -1,74 +1,99 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics.regression import r2_score, mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import AdaBoostRegressor
+# import GridSearchCV
 import xlrd
+import math
+import matplotlib.pyplot as plt
+import logging
+import os
+import sys
+import pandas as pd
+from sklearn import svm
+
+path_DBN = os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), "models"), "deep-belief-network")
+sys.path.append(path_DBN)
+from dbn.tensorflow import SupervisedDBNRegression
+
+np.random.seed(1337)  # for reproducibility
+logging.basicConfig(level=logging.INFO)
 
 
-def mytest(items, _use_all_data=True, _use_CCA_data=False, _use_pm25_history=True, _use_drop_least_importance=False,
-           _start=0, _end=1):
-    assert not (_use_all_data and _use_CCA_data)
-    assert _use_all_data or _use_CCA_data or _use_drop_least_importance or _use_pm25_history
-    assert (not _use_drop_least_importance) or (
-            (not _use_all_data) and (not _use_CCA_data) and _use_drop_least_importance)
+def train_model(learning_rate, periods, batch_size, feature, label, path_out_png):
+    X_train, X_test, Y_train, Y_test = train_test_split(feature, label, test_size=0.2, shuffle=False)
 
-    if not use_pm25_history:
-        items.pop('pm25')
-    if use_drop_least_importance:
-        items.pop('weather')
-        items.pop('wind')
-    elif use_CCA_data:  # pm10,co,temperature,moisture
-        items.pop('hour')
-        items.pop('o3')
-        items.pop('so2')
-        items.pop('no2')
-        items.pop('wind')
-        items.pop('weather')
-        items.pop('pressure')
-        items.pop('precipitation')
+    train_steps_per_period = X_train.shape[0] // periods  # floor
+    test_steps_per_period = X_test.shape[0] // periods
+    # print(X_train)
+    # print(Y_train)
 
-    re = []
-    for item in items.values():
-        re.append(item[_start:_end])
-    re = np.array(re)
-    re = re.reshape(re.size)
-    return re
+    '''regressor_DBN = SupervisedDBNRegression(hidden_layers_structure=[100],
+                                            learning_rate_rbm=0.01,
+                                            learning_rate=learning_rate,
+                                            n_epochs_rbm=20,
+                                            n_iter_backprop=200,
+                                            batch_size=batch_size,
+                                            activation_function='sigmoid',
+                                            verbose=False)'''
+    regressor_DBN = SupervisedDBNRegression(learning_rate=learning_rate, batch_size=batch_size, verbose=False)
+
+    print("Training model...")
+    print("RMSE (on training data):")
+    root_mean_squared_errors = []
+    for period in range(0, periods):
+        x_train = np.array(X_train[period * train_steps_per_period:(period + 1) * train_steps_per_period])
+        # x_train=x_train.reshape(x_train.size,1)
+        y_trian = np.array(Y_train[period * train_steps_per_period:(period + 1) * train_steps_per_period])
+        # y_trian=y_trian.reshape(y_trian.size,1,1)
+        # print(x_train)
+        # print(y_trian)
+        regressor_DBN.fit(x_train, y_trian)
+
+        x_test = X_test[period * test_steps_per_period:(period + 1) * test_steps_per_period]
+        y_test = Y_test[period * test_steps_per_period:(period + 1) * test_steps_per_period]
+        predictions = regressor_DBN.predict(x_test)
+        # predictions = np.array([predictions])
+        # print(predictions.shape)
+        # print(y_test.shape)
+
+        root_mean_squared_error = math.sqrt(mean_squared_error(y_test, predictions))
+
+        print("  period %02d : %0.2f" % (period, root_mean_squared_error))
+
+        root_mean_squared_errors.append(root_mean_squared_error)
+
+    print("Model training finished.")
+
+    # Output a graph of loss metrics over periods.
+    plt.subplot(1, 2, 2)
+    plt.ylabel('RMSE')
+    plt.xlabel('Periods')
+    plt.title("Root Mean Squared Error vs. Periods")
+    plt.tight_layout()
+    plt.plot(root_mean_squared_errors)
+    plt.savefig(path_out_png)
+
+    print("Final RMSE (on training data): %0.2f" % root_mean_squared_error)
 
 
-path_data = "data/airdata.xlsx"
+path_data = "data/airdata.csv"
+# path_out_txt = "out/out_test.txt"
+path_out_png = "out/out_test.png"
 
-data = xlrd.open_workbook(path_data)
-table = data.sheet_by_index(0)
-data_num = table.nrows - 1
+data = pd.read_csv(path_data, sep=",")
 
-hour = np.array(table.col_values(1)[1:])
-pm25 = np.array(table.col_values(2)[1:])
-o3 = np.array(table.col_values(3)[1:])
-pm10 = np.array(table.col_values(4)[1:])
-so2 = np.array(table.col_values(5)[1:])
-no2 = np.array(table.col_values(6)[1:])
-co = np.array(table.col_values(7)[1:])
-temperature = np.array(table.col_values(8)[1:])
-wind = np.array(table.col_values(9)[1:])
-weather = np.array(table.col_values(10)[1:])
-moisture = np.array(table.col_values(11)[1:])
-pressure = np.array(table.col_values(12)[1:])
-precipitation = np.array(table.col_values(13)[1:])
+target = data["pm25"]
+target = target.drop([0])
 
-use_all_data = True
-use_CCA_data = False
-use_pm25_history = True
-use_drop_least_importance = False
+data = data.drop([data.shape[0] - 1])
+data = data.drop(["date"], axis=1)
+# print(data["date"])
 
-step = 2
 
-re_data = mytest(
-    {"hour": hour, "pm25": pm25, "o3": o3, "pm10": pm10, "so2": so2, "no2": no2, "co": co, "temperature": temperature,
-     "wind": wind, "weather": weather, "moisture": moisture, "pressure": pressure, "precipitation": precipitation},
-    _use_all_data=use_all_data, _use_CCA_data=use_CCA_data, _use_pm25_history=use_pm25_history,
-    _use_drop_least_importance=use_drop_least_importance, _start=0, _end=step)
-print(re_data)
-print(re_data.shape)
+learning_rate = 0.00001
+batch_size = 1
 
-Data = np.concatenate((hour[0:step], pm25[0:step], o3[0:step], pm10[0:step], so2[0:step], no2[0:step],
-                       co[0:step], temperature[0:step], wind[0:step], weather[0:step], moisture[0:step],
-                       pressure[0:step], precipitation[0:step]), axis=0)
-print(Data)
-print(Data.shape)
+train_model(learning_rate=learning_rate, periods=10, batch_size=batch_size, feature=data, label=target,
+            path_out_png=path_out_png)
